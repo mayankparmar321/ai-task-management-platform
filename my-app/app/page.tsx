2b7428ef848ea719563608ceea7eec89303feaa9
mayank parmar <mayank.jm.123@gmail.com>
2026-07-19 18:02:05 +0530

    allowing import

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,8 +1,21 @@
+import { json } from 'stream/consumers';
+import { prisma } from '../src/db'
+export default async function Home() {
+  
+ const user = await prisma.user.findUnique({
+  where: { id:2}
+ })
+ const task = await prisma.task.findFirst({
+  where: {id:1}
+ })
+ 
 
-export default function Home() {
+  
   return (
     <div>
-      
+      {user?.name}
+      {task?.title}
     </div>
   );
 }
+
